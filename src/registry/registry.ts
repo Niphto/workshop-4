8ca6bd14288f8398ab766dd34bc2a80a37425f6c
unknown <adamnassir2001@gmail.com>
2024-03-26 17:57:04 +0100

    Work Done

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -3,28 +3,29 @@ import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
 
 export type Node = { nodeId: number; pubKey: string };
-
-export type RegisterNodeBody = {
-  nodeId: number;
-  pubKey: string;
-};
+export type RegisterNodeBody = { nodeId: number; pubKey: string; };
 
 export type GetNodeRegistryBody = {
-  nodes: Node[];
-};
+  nodes: Node[]; };
 
 export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
-
+  let nodes: Node[] = [];
   _registry.get("/status", (req, res) => {
-    res.send("live");
-  });
-
+    res.send("live");});
+  _registry.post("/registerNode", (req, res) => {
+    const { nodeId, pubKey }: RegisterNodeBody = req.body;
+    const nodeExists = nodes.find(node => node.nodeId === nodeId);
+    if (nodeExists) {
+      return res.status(400).json({ message: "Node already registered." });}
+    nodes.push({ nodeId, pubKey });
+    return res.status(201).json({ message: "Node registered successfully." });});
+  _registry.get("/getNodeRegistry", (req: Request, res: Response) => {
+    const payload: GetNodeRegistryBody = {
+      nodes: nodes};
+    res.json(payload);});
   const server = _registry.listen(REGISTRY_PORT, () => {
-    console.log(`registry is listening on port ${REGISTRY_PORT}`);
-  });
-
-  return server;
-}
+    console.log(`Registry is listening on port ${REGISTRY_PORT}`);});
+  return server;}
